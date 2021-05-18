acd8856a910084874c07bec0d5906bdae1d9dfd5
ho-taek <kht072623@naver.com>
2021-05-19 01:55:13 +0900

    [FEAT] #5 - homeViewPager2

--- app/src/main/java/org/sopt/ui/adapter/SampleAdapter.kt
+++ app/src/main/java/org/sopt/ui/adapter/SampleAdapter.kt
@@ -1,5 +1,31 @@
 package org.sopt.ui.adapter
 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import org.sopt.ui.view.home.tab.BestTab
+import org.sopt.ui.view.home.tab.BrandTab
+import org.sopt.ui.view.home.tab.HomeTab
+import org.sopt.ui.view.home.tab.NewTab
+
 class SampleAdapter {
-    //어댑터 클래스는 다 여기에 넣어주세요
+
+    class PagerFragmentStateAdapter(fm: FragmentActivity): FragmentStateAdapter(fm){
+
+
+        override fun getItemCount(): Int {
+            return 4
+        }
+        override fun createFragment(position: Int): Fragment {
+            return when(position){
+                0 ->(HomeTab())
+                1 ->(BrandTab())
+                2 ->(BestTab())
+                else ->(NewTab())
+            }
+        }
+
+
+    }
+
 }
