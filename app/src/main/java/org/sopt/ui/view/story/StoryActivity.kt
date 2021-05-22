348b9fc501ad1d4e7f6d0600cd35bcf253d4346b
dustn1259 <dustn1259@naver.com>
2021-05-22 13:41:13 +0900

    activity

--- app/src/main/java/org/sopt/ui/view/story/StoryActivity.kt
+++ app/src/main/java/org/sopt/ui/view/story/StoryActivity.kt
@@ -1,8 +1,13 @@
 package org.sopt.ui.view.story
 
+import android.animation.ObjectAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import org.sopt.databinding.ActivityStoryBinding
+import org.sopt.ui.view.store.StoreFragment
 
 class StoryActivity : AppCompatActivity() {
     lateinit var binding : ActivityStoryBinding
@@ -12,5 +17,28 @@ class StoryActivity : AppCompatActivity() {
         binding = ActivityStoryBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        binding.progressBar.max = 1000
+
+        val currentProgress = 1000
+
+        ObjectAnimator.ofInt(binding.progressBar,"progress",currentProgress)
+            .setDuration(15000)
+            .start()
+
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            finish()
+        }, 15000)
+
+        val picIntent: Intent = getIntent()
+        binding.Ellipsepic.setImageResource(picIntent.getIntExtra("img",1))
+
+
+
+        binding.closeStroyBtn.setOnClickListener {
+            finish()
+        }
     }
 }
