c49cd72443340ecaaa89396bd0f17b4d2cc66a1e
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-06-03 01:51:59 +0900

    [DEL] #16 - delete useless local datasource and model

--- app/src/main/java/org/sopt/ui/view/store/data/datasource/StoreDataSource.kt
+++
@@ -1,9 +0,0 @@
-package org.sopt.ui.view.store.data.datasource
-
-import org.sopt.ui.view.store.data.model.BrandData
-import org.sopt.ui.view.store.data.model.StoryData
-
-interface StoreDataSource {
-    fun getBrandData(): List<BrandData>
-    fun getStoryData(): List<StoryData>
-}
